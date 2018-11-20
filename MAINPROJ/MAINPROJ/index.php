56a473ad022921c09defd09e030e8b25ccd1fb55
Rakshith kakathkar <rakshithkakathkar@gmail.com>
2018-11-20 08:06:23 +0530

    Changes can be made

---
+++ MAINPROJ/MAINPROJ/index.php
@@ -0,0 +1,33 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CrAzY TuNeS- Home</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    </head>
+    <body>
+        
+    </body>
+    <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="artist.php">Artist</a></li>
+                    <li><a class="p">CrAzY TuNeS</a></li>
+                    <li><a href="user.php">Music</a></li>                  
+                </ul>
+        </nav>
+    <div id="content_area">
+        <h1><a class="v"> CrAzY TuNeS</a> </h1>
+        <h1><a class="u">What is Music? Music is the ‘art of combining tones in a manner to please the ear’. </a></h1>
+    </div>
+     
+             
+                       
+                                    <img src="images/mn.jpg" class="imgdown" />
+                                     <img src="images/m1.jpg" class="imgdown2" />
+                                      <img src="images/m2.jpg" class="imgdown" />
+                                     <img src="images/m3.jpg" class="imgdown2" />
+                                                                           
+                                    
+                                       
+            
+</html>
